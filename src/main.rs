e8bd519fa12ceb359e34b7ad6cee8bdf8855b10e
Jonathan Wells <jnw72@cornell.edu>
2024-10-05 15:48:33 -0400

    Warn about multiple fasta file

--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,49 @@ use seqalign::{Aligner, parse_fastafile};
 #[command(version, about)]
 struct Args {
     #[arg(short, long)]
-    asequence: String,
+    afile: String,
 
     #[arg(short, long)]
-    bsequence: String,
+    bfile: String,
 
     #[arg(short, long)]
     scoring_matrix: String,
 
-    #[arg(short, long)]
+    #[arg(short='o', long)]
     open_penalty: i32,
 
     #[arg(short, long)]
     extension_penalty: i32,
+
+    #[arg(short='t', long)]
+    output: String,
+
 }
 
 fn main() -> Result<(), Error> {
     let args = Args::parse();
 
     let aligner = Aligner::new(
-        args.scoring_matrix.as_str(),
+        &args.scoring_matrix,
         args.open_penalty,
         args.extension_penalty
         )?;
     
-    let arecords = parse_fastafile(&args.asequence)?;
-    let brecords = parse_fastafile(&args.bsequence)?;
-    let asequence = &arecords[0];
-    let bsequence = &brecords[0];
-    let aresult = aligner.align(&asequence.seq, &bsequence.seq);
+    let arecords = parse_fastafile(&args.afile)?;
+    let brecords = parse_fastafile(&args.bfile)?;
+    
+    if arecords.len() > 1 || brecords.len() >  1 {
+        eprintln!("WARNING: seqalign currently only accepts single pairs.\n\
+                  Defaulting to first sequence per file.")
+    }
+    // TODO: extend to make this accept multi-sequence files.
+    let afile = &arecords[0];
+    let bfile = &brecords[0];
+    let aresult = aligner.align(&afile.seq, &bfile.seq);
     
     let alignment = aresult.alignment();
-    println!(">{}\n{}", asequence.name, alignment.0);
-    println!(">{}\n{}", bsequence.name, alignment.1);
+    println!(">{}\n{}", afile.name, alignment.0);
+    println!(">{}\n{}", bfile.name, alignment.1);
     
     Ok(())
 }
